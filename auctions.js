c2f4ca386803cf6cae379bf417027a868030af58
Brian Stadnicki <stadnickibrian@gmail.com>
2021-07-27 20:34:14 +0100

    setup loading all lots

--- auctions.js
+++ auctions.js
@@ -15,14 +15,34 @@ function loadAuctions() {
         // parse index page
         const indexParser = new DOMParser()
         const indexDoc = indexParser.parseFromString(indexData, "text/html")
-        // get auctions table and append to #select-auction
+        // get auctions table and show
         const auctionsTable = indexDoc.querySelector("body > div.container > div > div:nth-child(3) > table:nth-child(1)")
         $("body").append(auctionsTable)
     })
 }
 
 function loadLots() {
+    loadLotsPage(1, true)
+}
 
+function loadLotsPage(page, auto_continue) {
+    return $.get("https://cors-anywhere.herokuapp.com/https://www.johnpyeauctions.co.uk/lot_list.asp?" + window.location.search.substring(1) + "&pageno=" + page, function (lotsData) {
+        // parse lots page
+        const lotsParser = new DOMParser()
+        const lotsDoc = lotsParser.parseFromString(lotsData, "text/html")
+        // get lots table and show
+        const lotsTable = lotsDoc.querySelector("body > div.container > div > div:nth-child(2) > center > table > tbody > tr:nth-child(2) > td > div:nth-child(5) > table")
+        $("body").append(lotsTable)
+        // automatically continue if should
+        if (auto_continue) {
+            // check if there is a next link
+            if (lotsData.includes("&pageno=" + (page + 1))) {
+                loadLotsPage(page + 1, true)
+            }
+        }
+    }).fail(function() {
+        console.log("Failed to fetch lots page " + page)
+    })
 }
 
 function getUrlParameter(sParam) {
