1896dad2d3c3982570aa5e22192dae49f791d039
Brian Stadnicki <stadnickibrian@gmail.com>
2021-08-23 02:15:38 +0100

    save if favoured to local storage

--- auctions.js
+++ auctions.js
@@ -80,10 +80,24 @@ function parseLot(lot) {
 
     // lot number
     let lotNumber = lot.querySelector("td:nth-child(1) > a > h5").textContent
-    lotEntry.append("<th scope='row' style='position: relative'><h3>" + lotNumber + "<br>" +
-        "<svg style='position: absolute; top: 50%' onclick='favouriteLot(" + lotNumber + ")'  xmlns=\"http://www.w3.org/2000/svg\" width=\"32\" height=\"32\" fill=\"currentColor\" class=\"bi bi-star clickable\" viewBox=\"0 0 16 16\">\n" +
-        "  <path d=\"M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z\"/>\n" +
-        "</svg>" + "</h3></th>")
+    let lotStorage = JSON.parse(window.localStorage.getItem("lot-" + lotNumber))
+    if (lotStorage && lotStorage.favourite) {
+        lotEntry.append("<th scope='row' style='position: relative'><h3>" + lotNumber + "<br>" +
+            "<svg style='position: absolute; top: 50%' onclick='unfavouredLot(" + lotNumber + ")' xmlns=\"http://www.w3.org/2000/svg\" width=\"32\" height=\"32\" fill=\"currentColor\" class=\"bi bi-star-fill clickable\" viewBox=\"0 0 16 16\">\n" +
+            "  <path d=\"M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z\"/>\n" +
+            "</svg>" + "</h3></th>")
+    } else {
+        lotEntry.append("<th scope='row' style='position: relative'><h3>" + lotNumber + "<br>" +
+            "<svg style='position: absolute; top: 50%' onclick='favouriteLot(" + lotNumber + ")'  xmlns=\"http://www.w3.org/2000/svg\" width=\"32\" height=\"32\" fill=\"currentColor\" class=\"bi bi-star clickable\" viewBox=\"0 0 16 16\">\n" +
+            "  <path d=\"M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z\"/>\n" +
+            "</svg>" + "</h3></th>")
+        if (!lotStorage) {
+            const lotJSON = {
+                favoured: false
+            }
+            window.localStorage.setItem("lot-" + lotNumber, JSON.stringify(lotJSON))
+        }
+    }
 
     // add lot number as id to lot entry
     lotEntry.attr("id", "lot-" + lotNumber)
@@ -152,12 +166,18 @@ function favouriteLot(lot) {
     $('#lot-' + lot + ' > th > h3 > svg').replaceWith("<svg style='position: absolute; top: 50%' onclick='unfavouredLot(" + lot + ")' xmlns=\"http://www.w3.org/2000/svg\" width=\"32\" height=\"32\" fill=\"currentColor\" class=\"bi bi-star-fill clickable\" viewBox=\"0 0 16 16\">\n" +
         "  <path d=\"M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z\"/>\n" +
         "</svg>")
+    const lotJSON = JSON.parse(window.localStorage.getItem("lot-" + lot))
+    lotJSON.favoured = true
+    window.localStorage.setItem("lot-" + lot, JSON.stringify(lotJSON))
 }
 
 function unfavouredLot(lot) {
     $('#lot-' + lot + ' > th > h3 > svg').replaceWith("<svg style='position: absolute; top: 50%' onclick='favouriteLot(" + lot + ")'  xmlns=\"http://www.w3.org/2000/svg\" width=\"32\" height=\"32\" fill=\"currentColor\" class=\"bi bi-star clickable\" viewBox=\"0 0 16 16\">\n" +
         "  <path d=\"M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z\"/>\n" +
         "</svg>")
+    const lotJSON = JSON.parse(window.localStorage.getItem("lot-" + lot))
+    lotJSON.favoured = false
+    window.localStorage.setItem("lot-" + lot, JSON.stringify(lotJSON))
 }
 
 function getUrlParameter(sParam) {
